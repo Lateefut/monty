aac036ff36f0c53bf36473fbb60a4f63173299d6
Adebanjo Lateef <adebanjolateef18@gmail.com>
2022-07-13 04:55:40 -0700

    monty header

--- monty.h
+++ monty.h
@@ -13,7 +13,7 @@
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
+ * @n: integers
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
  *
