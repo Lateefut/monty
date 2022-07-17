4712b01e01a72adc52ade3c2d5e4af928033ead2
Adebanjo Lateef <adebanjolateef18@gmail.com>
2022-07-17 12:48:02 +0100

    stack to be freed

---
+++ free.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+/**
+ *free_stack - check the codes.
+ *@st: stack to be freed
+ * Return: Always 0.
+ */
+void free_stack(stack_t **st)
+{
+	stack_t *next = *st;
+
+	while (next)
+	{
+		next = (*st)->next;
+		free(*st);
+		*st = next;
+	}
+
+}
+/**
+ *free_all - Free all the project vars used.
+ *@stack: stack to be freed
+ * Return: void
+ */
+void free_all(stack_t **stack)
+{
+	free_stack(stack);
+	if (m_var.buff)
+		free(m_var.buff);
+	if (m_var.fd)
+		fclose(m_var.fd);
+}
