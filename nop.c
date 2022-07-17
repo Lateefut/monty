39d021220bba07c3c1073f1472159ea1159f3616
Adebanjo Lateef <adebanjolateef18@gmail.com>
2022-07-17 12:39:50 +0100

    fixes

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ *_nop - check the code.
+ *@st: stack to be freed
+ *@line_number: line number
+ * Return: Always 0.
+ */
+void _nop(stack_t **st, unsigned int line_number)
+{
+	(void) st;
+	(void) line_number;
+}
