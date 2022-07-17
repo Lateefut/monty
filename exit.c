68de581b05502616815b09a246942c9e1f59e9dd
Adebanjo Lateef <adebanjolateef18@gmail.com>
2022-07-17 12:48:59 +0100

    unified frees

---
+++ exit.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * exit_failure - check the code.
+ *@st: stack to be freed
+ *@mess: out message
+ * Return: Always 0.
+ */
+void exit_failure(stack_t **st, const char *mess)
+{
+	fprintf(stderr, mess, m_var.line);
+	free_all(st);
+	exit(EXIT_FAILURE);
+}
